1f3db70b12643983654893491751673a9947257c
fatimatirras <fatmaturrash@gmail.com>
2021-01-02 15:23:33 +0300

    category düzenleme

--- resources/views/admin/category_edit.blade.php
+++ resources/views/admin/category_edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 
-@section('title','Add Category')
+@section('title','Edit Category')
 
 @section('content')
 
@@ -8,7 +8,7 @@
         <div class="container-fluid">
             <ul class="breadcrumb">
                 <li class="breadcrumb-item"><a href="{{ route('admin_home') }}">Home</a></li>
-                <li class="breadcrumb-item active">Add Category      </li>
+                <li class="breadcrumb-item active">Edit Category      </li>
             </ul>
         </div>
     </div>
@@ -16,18 +16,19 @@
     <div class="card-body">
 
                  <div>
-                     <p>Add Category</p>
+                     <p>Edit Category</p>
                  </div>
-                    <form action="{{ route('admin_category_create')}}" method="post">
+                    <form action="{{ route('admin_category_update',['id'=>$data->id])}}" method="post">
                         @csrf
                         <div class="form-group">
                             <label>Parent</label>
 
-                            <select  class="form-control" name="parent_id">
-                                <option value="o" selected="selected">Main Category</option>
+                            <select  class="form-control" name="parent_id" style="...">
+
+                                <option value="o" >Main Category</option>
 
                                 @foreach( $datalist as $rs)
-                                <option value="{{ $rs->id }}">{{ $rs->title }}</option>
+                                <option value="{{ $rs->id }}"  @if ($rs->id == $data->parent_id) selected="selected" @endif >{{ $rs->title }}</option>
                                 @endforeach
                             </select>
 
@@ -36,23 +37,24 @@
 
                         <div class="form-group">
                             <label>Title</label>
-                            <input type="text" name="title"  class="form-control">
+                            <input type="text" name="title" value="{{$data->title}}"  class="form-control">
                         </div>
 
                         <div class="form-group">
                             <label>Keywords</label>
-                            <input type="text" name="keywords"  class="form-control">
+                            <input type="text" name="keywords" value="{{$data->keywords}}"  class="form-control">
                         </div>
 
                         <div class="form-group">
                             <label>Description</label>
-                            <input type="text" name="description"  class="form-control">
+                            <input type="text" name="description"  value="{{$data->description}}" class="form-control">
                         </div>
 
                         <div class="form-group row">
                             <label class="col-sm-2 form-control-label">Status</label>
                             <div class="col-sm-10 mb-3">
                                 <select name="status" class="form-control" >
+                                    <option selected="selected">{{$data->status}}</option>
                                     <option>False</option>
                                     <option>True</option>
                                 </select>
@@ -61,7 +63,7 @@
                         </div>
 
                         <div class="form-group">
-                            <input type="submit" value="Add category" class="btn btn-primary">
+                            <input type="submit" value="Update category" class="btn btn-primary">
 
                         </div>
                     </form>
