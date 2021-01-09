c5baf04e41f96f9aa32813d27fc004428aa211ca
fatimatirras <fatmaturrash@gmail.com>
2021-01-09 22:19:21 +0300

    menu content One to Many ilişkisi

--- resources/views/admin/content_add.blade.php
+++ resources/views/admin/content_add.blade.php
@@ -19,41 +19,30 @@
         </div>
     </div>
 
-    <div class="card-body">
+            <div class="card-body">
 
                     <form action="{{ route('admin_content_store')}}" method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group">
                             <label>Menu</label>
-
                             <select  class="form-control" name="menu_id">
-
                                 @foreach( $datalist as $rs)
                                 <option value="{{ $rs->id }}">{{ $rs->title }}</option>
                                 @endforeach
                             </select>
-
                         </div>
-
-
                         <div class="form-group">
                             <label>Title</label>
                             <input type="text" name="title"  class="form-control">
                         </div>
-
                         <div class="form-group">
                             <label>Keywords</label>
                             <input type="text" name="keywords"  class="form-control">
                         </div>
-
                         <div class="form-group">
                             <label>Description</label>
                             <input type="text" name="description"  class="form-control">
                         </div>
-
-
-
-
                         <div class="form-group">
                             <label>Detail</label>
                             <textarea name="detail"></textarea>
@@ -61,27 +50,22 @@
                                 CKEDITOR.replace( 'detail' );
                             </script>
                         </div>
-
                         <div class="form-group">
                             <label>Apartments</label>
                             <input type="text" name="apartments"  class="form-control">
                         </div>
-
                         <div class="form-group">
                             <label>News</label>
                             <input type="text" name="news"  class="form-control">
                         </div>
-
                         <div class="form-group">
                             <label>Announcement</label>
                             <input type="text" name="announcement"  class="form-control">
                         </div>
-
                         <div class="form-group">
                             <label>Image</label>
                             <input type="file" name="image"  class="form-control">
                         </div>
-
                         <div class="form-group row">
                             <label class="col-sm-2 form-control-label">Status</label>
                             <div class="col-sm-12 mb-5">
@@ -90,19 +74,12 @@
                                     <option>True</option>
                                 </select>
                             </div>
-
                         </div>
-
                         <div class="form-group">
                             <input type="submit" value="Add content" class="btn btn-primary">
-
                         </div>
                     </form>
-
-
-
-
-    </div>
+            </div>
 
 
 @endsection
